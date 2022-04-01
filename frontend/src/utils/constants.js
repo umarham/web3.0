d4b7d05fc8e6534652feb7772aa1119323e933c8
Umar <umarhameed943@gmail.com>
2022-04-01 21:11:20 +0200

    web3

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,6 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+
+
+export const contractAddress = "0xB5484C23f2036412898F2c586dB026aB17D87bDd";
---
+++ frontend/src/utils/dummyData.js
@@ -0,0 +1,30 @@
+
+export default [
+    {
+      id: 1,
+      url: "https://i.pinimg.com/originals/68/a0/9e/68a09e774e98242871c2db0f99307420.gif",
+      message: "",
+      timestamp: "28/3/2022, 5:59:09 PM",
+      addressFrom: "0xCF8e569A97C423952DdFf902375C7C76549A6A90",
+      amount: "0.01",
+      addressTo: "0x8aa395Ab97837576aF9cd6946C79024ef1acfdbE",
+    },
+    {
+      id: 2,
+      url: "https://i.pinimg.com/originals/73/d3/a1/73d3a14d212314ab1f7268b71d639c15.gif",
+      message: "",
+      timestamp: "28/3/2022, 12:00:00 Pm",
+      addressFrom: "0xCF8e569A97C423952DdFf902375C7C76549A6A90",
+      amount: "0.01",
+      addressTo: "0x8aa395Ab97837576aF9cd6946C79024ef1acfdbE",
+    },
+    {
+      id: 3,
+      url: "https://www.omnisend.com/blog/wp-content/uploads/2016/09/funny-gifs-9.gif",
+      message: "",
+      timestamp: "28/3/2022, 8:30:01 PM",
+      addressFrom: "0xCF8e569A97C423952DdFf902375C7C76549A6A90",
+      amount: "0.01",
+      addressTo: "0x8aa395Ab97837576aF9cd6946C79024ef1acfdbE",
+    },
+  ];
